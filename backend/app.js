04d0475731db25deb87e219f18b0c8a369be1418
Omar Ahmed Aly <omarahmedd315@gmail.com>
2026-03-09 17:39:52 +0200

    new

--- backend/app.js
+++ backend/app.js
@@ -6,3 +6,4 @@ const app = express();
 // This is the crucial line missing! 
 // It hands the 'app' over so that index.js can grab it.
 module.exports = app;
+// Note: You don't need to call app.listen() here, because index.js will do that for you.
